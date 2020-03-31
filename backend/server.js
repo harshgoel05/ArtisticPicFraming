5f4b44520e99ec52b987d03d9b64df62e0a4005e
sanskar agrawal <agrawalsanskar9993@gmail.com>
2020-03-31 18:12:29 +0530

    adress_form

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express=require('express')
 const app=express()
 const cors=require('cors')
+const qr=require('qr-image')
+const fs = require('fs');
+const qr1 = require('qr-encode')
 const bodyParser=require('body-parser')
 const api=require('./api')
 const uuid=require('uuid/v4')
 const nodemailer = require('nodemailer');
-
-
 const stripe = require('stripe')('sk_test_9UhQivpSo4yuuExMR7ouzfei00Z3EieXgc');
 const exphbs = require('express-handlebars');
 app.use(cors());
@@ -24,13 +25,10 @@ var transporter = nodemailer.createTransport({
     pass: process.env.PASSWORD  
   }
 });
+app.use(express.static('./public'))
+var receipt_url;
 
-var mailOptions = {
-  from: 'predatesan@gmail.com',
-  to: 'sanskarag23@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
+
 
 
 
@@ -63,26 +61,47 @@ app.get('/payment', (req, res) => {
     })
     .then(customer => {stripe.charges.create({
       amount,
-      
+       
       description: `ArtisticPicFraming`,
       currency: 'usd',
       customer: customer.id,
       shipping: {
-        name: 'Jenny Rosen',
-        address: {
-          line1: '510 Townsend St',
-          postal_code: '98140',
-          city: 'San Francisco',
-          state: 'CA',
-          country: 'US',
-        }}},{idempotencyKey}
+        name: req.body.name,
+        address: req.body.address}},{idempotencyKey}
     
     ).catch(err=>{console.log(err);
-    res.send({'payment':'unsuccess'})
+    res.send({'payment':false})
     }
     )
-    .then(charge => {res.status(200).send({'payment':'success'})
-    console.log(charge)
+    .then(charge => {
+      if(charge.status=='succeeded')
+      res.status(200).send({'payment':true})
+      console.log(charge)
+    var qr_png = qr.imageSync('price:'+amount+'\nProduct name:ArtisticFrame', { type: 'png' });
+    let qr_name='new1.png'
+    fs.writeFileSync('./public/' + qr_name,qr_png , (err) => {
+        
+      if(err){
+      console.log(err);
+     }
+  
+     })
+
+      var mailOptions = {
+        from: 'predatesan@gmail.com',
+        to: req.body.stripeEmail,
+        subject: 'Order Confirmation',
+        text: 'You order has been succefully confirmed\n'+charge.receipt_url,
+        html:'<h1>Scan the code</h1><br><img src="cid:img1"><style>h1{margin:20px;}</style><a href="'+charge.receipt_url+'"><button>Receipt</button></a>',
+        attachments:[
+            {
+              filename:'new1.png',
+              path:'./public/new1.png',
+              cid:'img1'
+
+            }
+        ]
+      };
     transporter.sendMail(mailOptions, function(error, info){
       if (error) {
         console.log(error);
@@ -91,6 +110,8 @@ app.get('/payment', (req, res) => {
       }
     });
   })
+  
+  
     
   
 
